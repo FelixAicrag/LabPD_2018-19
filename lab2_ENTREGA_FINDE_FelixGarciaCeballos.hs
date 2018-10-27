0213c503530c17abb71fc9b4bb1886bba7cdb4de
Felix Garcia Ceballos <36733778+FelixAicrag@users.noreply.github.com>
2018-10-27 20:27:28 +0200

    v2
    ya falta menos...

--- lab2_ENTREGA_FINDE_FelixGarciaCeballos.hs
+++ lab2_ENTREGA_FINDE_FelixGarciaCeballos.hs
@@ -45,9 +45,39 @@ par x
  | mod x 2 == 0 = True
  | otherwise    = False
 
+--EJERCICIO 4:
+foldlast :: [a] -> a
+foldlast (x:xs) = foldl (\y x -> x) x xs
 
+foldreverse :: [a] -> [a]
+foldreverse bs = foldr (\b g x -> g (b : x)) id bs []
 
+foldall, foldany :: (a -> Bool) -> [a] -> Bool
+foldall f xs = foldl (&&) True (map f xs)
+foldany f xs = foldl (||) False (map f xs)
 
+foldmin :: (Ord a) => [a] -> a
+foldmin (x:xs) = foldl min x xs
+
+foldmap :: (a -> b) -> [a] -> [b]
+foldmap f (x:xs) = foldr (\y ys -> (f y):ys) [] (x:xs)
+
+foldfilter, foldtwhile :: (a -> Bool) -> [a] -> [a]
+foldfilter f (x:xs) = foldr (\y ys -> if f y then y:ys else ys) [] (x:xs)
+foldtwhile f (x:xs) = foldr (\y ys -> if f y then y:ys else []) [] (x:xs)
+
+foldtake :: Int -> [a] -> [a]
+foldtake n xs = foldr step (const []) xs n
+  where
+    step x g 0 = []
+    step x g n = x:g (n-1)
+
+foldconcat :: [a] -> [a] -> [a]
+foldconcat xs ys = foldr (\x y -> x:y) ys xs
+
+--EJERCICIO 5:
+
+ 
 --EJERCICIO 6:
 prefixes, suffixes, sublists, perms, inits  :: [a] -> [[a]]
 prefixes xs = [take n xs | n <- [0..length xs]]
@@ -63,6 +93,10 @@ perms xs0 = xs0 : perms' xs0 []
             intercalar' f (y:ys) r = let (us,zs) = intercalar' (f . (y:)) ys r
                                      in  (y:us, f (x:y:us) : zs)
 inits xs = [take n xs | n <- [0..(length xs - 1)]]
+
+
+--EJERCICIO 7:
+
 
 
 --Funciones de List
